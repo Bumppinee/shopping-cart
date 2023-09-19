94c7def294f790aa3e45c451f211a9047d3d2f85
Bumppinee <rbrisbanenupe@yahoo.com>
2023-09-19 16:44:33 -0400

    components and header 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import {BrowserRouter, Route, Routes }from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home'
+import Cart from './components/Cart'
 
 function App() {
   return (
-    <div className="App">
+   <BrowserRouter>
       <Header/>
-    </div>
+      <Routes>
+        <Route exact path ='/' element={<Home/>}/>
+        <Route exact path ='/cart' element={<Cart/>}/>
+          
+
+       
+        
+      </Routes>
+      </BrowserRouter>
+    
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,39 @@
 import React from 'react'
-import {Container, Navbar} from 'react-bootstrap'
+import {Badge, Container, Dropdown, FormControl, Nav, Navbar} from 'react-bootstrap'
+import {FaShoppingCart} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
 
 
 
 // Container keeps everything aligned to the center
+// Drop down aligned to Right
+// npm i react dom help us to create different pages in react.js
 const Header = () => {
   return (
-    <Navbar>
+    <Navbar bg='dark' variant='dark' style={{height:80}}>
       <Container>
         <Navbar.Brand>
-          <a>Shopping Cart</a>
+         <Link to='/'>Shopping Cart</Link>
         </Navbar.Brand>
+        <Navbar.Text className='search'>
+          <FormControl style={{width: 500}} placeholder='Search for Item'
+          className='m-auto'/>
+        </Navbar.Text>
+        <Nav>
 
+          <Dropdown alignRight>
+            <Dropdown.Toggle variant='success'>
+              {<FaShoppingCart color='white' fontSize='25px' /> } 
+              <Badge>{10}</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu style={{minWidth: 370}}>
+              <span style={{paddin: 10}}>Cart is Empty!</span>
+
+            </Dropdown.Menu>
+
+          </Dropdown>
+        </Nav>
 
       </Container>
 
@@ -19,4 +41,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default Header;
